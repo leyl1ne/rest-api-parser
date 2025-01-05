6f115db3410331212c2381ce35eb0890165da609
leyl1ne <e-khodin@inbox.ru>
2025-01-05 16:21:42 +0300

    create handler get and test

---
+++ pkg/http-server/handlers/song/get/get.go
@@ -0,0 +1,81 @@
+package get
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	resp "github.com/leyl1ne/rest-api-parser/pkg/api/response"
+	"github.com/leyl1ne/rest-api-parser/pkg/models"
+	"github.com/leyl1ne/rest-api-parser/pkg/storage"
+)
+
+type Response struct {
+	resp.Response
+	Song models.Song
+}
+
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=SongGetter
+type SongGetter interface {
+	GetSong(id int) (models.Song, error)
+}
+
+func New(log *slog.Logger, songGetter SongGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.song.get.New"
+
+		log := log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		songID := chi.URLParam(r, "id")
+		if songID == "" {
+			log.Error("failed to get ID")
+
+			render.JSON(w, r, resp.Error("song ID is required"))
+
+			return
+		}
+
+		id, err := strconv.Atoi(songID)
+		if err != nil {
+			log.Error("failed to convert ID", slog.String("error", err.Error()))
+
+			render.JSON(w, r, resp.Error("invalid song ID"))
+
+			return
+		}
+
+		receivedSong, err := songGetter.GetSong(id)
+		if errors.Is(err, storage.ErrSongNotFound) {
+			log.Info("Song not found", slog.Int("id", id))
+
+			render.JSON(w, r, resp.Error("song not found"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to update song")
+
+			render.JSON(w, r, resp.Error("failed to update song"))
+
+			return
+		}
+
+		log.Info("song updated", slog.Int("id", id))
+		responseOK(w, r, receivedSong)
+
+	}
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request, song models.Song) {
+	render.JSON(w, r, Response{
+		Response: resp.OK(),
+		Song:     song,
+	})
+}
---
+++ pkg/http-server/handlers/song/get/get_test.go
@@ -0,0 +1,91 @@
+package get_test
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/leyl1ne/rest-api-parser/pkg/http-server/handlers/song/get"
+	"github.com/leyl1ne/rest-api-parser/pkg/http-server/handlers/song/get/mocks"
+	"github.com/leyl1ne/rest-api-parser/pkg/logger/handlers/slogdiscard"
+	"github.com/leyl1ne/rest-api-parser/pkg/models"
+	"github.com/leyl1ne/rest-api-parser/pkg/storage"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetHandler(t *testing.T) {
+	cases := []struct {
+		name       string
+		takeId     string
+		returnSong models.Song
+		respError  string
+		mockError  error
+	}{
+		{
+			name:   "Success",
+			takeId: "1",
+			returnSong: models.Song{
+				Title:  "Slim Shady",
+				Artist: "Eminem",
+				Lyrics: "Real slim shady",
+			},
+		},
+		{
+			name:      "Invalid ID",
+			takeId:    "ab",
+			respError: "invalid song ID",
+		},
+		{
+			name:      "Not Found Song",
+			takeId:    "1",
+			respError: "song not found",
+			mockError: storage.ErrSongNotFound,
+		},
+		{
+			name:      "Failed to update",
+			takeId:    "1",
+			respError: "failed to update song",
+			mockError: errors.New("unexpected error"),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			songGetterMock := mocks.NewSongGetter(t)
+
+			if tc.respError == "" || tc.mockError != nil {
+				songGetterMock.On("GetSong", 1).
+					Return(tc.returnSong, tc.mockError).
+					Once()
+			}
+
+			handler := get.New(slogdiscard.NewDiscardLogger(), songGetterMock)
+
+			req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/%s", tc.takeId), nil)
+			require.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+
+			r := chi.NewRouter()
+			r.Get("/{id}", handler.ServeHTTP)
+
+			r.ServeHTTP(rr, req)
+
+			require.Equal(t, http.StatusOK, rr.Code)
+
+			body := rr.Body.String()
+
+			var resp get.Response
+
+			require.NoError(t, json.Unmarshal([]byte(body), &resp))
+
+			require.Equal(t, tc.respError, resp.Error)
+
+		})
+	}
+}
